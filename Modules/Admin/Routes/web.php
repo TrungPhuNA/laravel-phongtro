6e9eb2a03d13f8cef30eb5e030165c9eeef42cc7
Phan Trung Phú <phupt@dol-tech.com>
2021-03-24 10:42:41 +0700

    add route and controller Admin

--- Modules/Admin/Routes/web.php
+++ Modules/Admin/Routes/web.php
@@ -13,4 +13,16 @@
 
 Route::prefix('admin')->group(function() {
     Route::get('/', 'AdminController@index');
+    Route::prefix('product')->group(function() {
+        Route::get('/', 'AdminProductController@index')->name('get_admin.product.index');
+    });
+    Route::prefix('user')->group(function() {
+        Route::get('/', 'AdminUserController@index')->name('get_admin.user.index');
+    });
+    Route::prefix('category')->group(function() {
+        Route::get('/', 'AdminCategoryController@index')->name('get_admin.category.index');
+    });
+    Route::prefix('location')->group(function() {
+        Route::get('/', 'AdminLocationController@index')->name('get_admin.location.index');
+    });
 });
---
+++ config/nav.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm .
+ * User: trungphuna .
+ * Date: 3/24/21 .
+ * Time: 10:35 AM .
+ */
+return [
+    'admin' => [
+        'nav' => [
+            [
+                'name' => 'Địa điểm',
+                'icon' => '',
+                'route' => 'get_admin.location.index'
+            ],
+            [
+                'name' => 'Danh mục',
+                'icon' => '',
+                'route' => 'get_admin.category.index'
+            ],
+            [
+                'name' => 'Sản phẩm',
+                'icon' => '',
+                'route' => 'get_admin.product.index'
+            ],
+        ]
+    ],
+    'user' => [
+
+    ]
+];
